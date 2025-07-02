6232086fbef86e9edd59202f2c166883c1841b69
Kevin <kevinksingh05@gmail.com>
2025-07-02 16:02:50 +0100

    Add files via upload
    solution to leetcode 704 binary search

---
+++ leet_704.py
@@ -0,0 +1,12 @@
+def search(nums, target):
+    left = 0
+    right = len(nums)
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid
+    return -1 
