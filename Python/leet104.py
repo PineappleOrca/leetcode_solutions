a7fbf70b9fe9d125d1b2f865edc4e8d8a23bf04c
PineappleOrca <kevinksingh05@gmail.com>
2026-04-18 07:08:12 +0100

    added solution to leet 104, max depth of tree

---
+++ Python/leet104.py
@@ -0,0 +1,16 @@
+from collections import deque 
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root: return 0
+        queue = deque([root])
+        level = 0
+        while queue:
+            for i in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            level += 1
+        return level
